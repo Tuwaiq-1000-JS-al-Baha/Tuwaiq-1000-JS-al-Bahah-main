652e75eedfe120ef19a07cdd8d7f58914c39a418
Sanobar-Fatema <91540886+Sanobar-Fatema@users.noreply.github.com>
2021-10-07 21:13:34 +0530

    Add files via upload

---
+++ week4/day5/React Bootstrap Meme App/src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Nav, Navbar, Button, Form, FormControl } from "react-bootstrap"
+
+function NavBar() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default NavBar
