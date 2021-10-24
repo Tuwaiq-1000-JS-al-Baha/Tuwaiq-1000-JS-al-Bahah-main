aa6bc07ec4737cdb3bad3ec48667788d473a6313
Adnen Ben Amor <91540820+adnnba@users.noreply.github.com>
2021-10-24 12:14:17 +0300

    Update script.js

--- week2/day5/Shopping Cart CODE/shopping-cart/script.js
+++ week2/day5/Shopping Cart CODE/shopping-cart/script.js
@@ -5,6 +5,7 @@ const total = document.querySelector(".total")
 
 addBtnList.forEach(addBtn => {
     addBtn.addEventListener("click", function () {
+        // get information from element
         const cardBody = addBtn.parentElement
         const title = cardBody.querySelector(".card-title").innerHTML
         const price = cardBody.querySelector(".card-subtitle span").innerHTML
@@ -12,18 +13,20 @@ addBtnList.forEach(addBtn => {
         console.log(price)
         const image = cardBody.previousElementSibling.src
         console.log(image)
-        const cartItem = '<div class="cart-item d-flex justify-content-between align-items-center">'
-            + '<img class="img-thumbnail" style="height: 100px;"'
-            + 'src="' + image + '"'
-            + 'alt="">'
-            + '<span>' + title + '</span>'
-            + '<span>Price: ' + price + ' SAR</span>'
-            + '</div>'
+        // create new element
+        const cartItem = `
+        <div class="cart-item d-flex justify-content-between align-items-center">
+            <img class="img-thumbnail" style="height: 100px;" src="${image}" alt="">
+            <span>${title}</span>
+            <span>Price: ${price}  SAR</span>
+        </div>`
+
+        // add new element to correct place
         cart.innerHTML += cartItem
+
+        // calculate the total
         const totalSpan = document.querySelector(".total span")
         totalSpan.innerHTML = Number(totalSpan.innerHTML) + Number(price)
-        total.innerHTML = totalSpan
-        console.log(total)
 
     })
 })
